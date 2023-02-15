a2bac4dd082acb0016404c814802c061d1584c23
VLV01 <vinicius.linsvasco@hotmail.com>
2023-02-15 17:39:11 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/VLV01/workshopmongo/config/Instantiation.java
+++ src/main/java/com/VLV01/workshopmongo/config/Instantiation.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.TimeZone;
 
 import com.VLV01.workshopmongo.domain.Post;
+import com.VLV01.workshopmongo.dto.AuthorDTO;
 import com.VLV01.workshopmongo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -38,10 +39,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
