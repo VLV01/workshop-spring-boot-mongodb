9d919d73062793a23724cf96ea37b73eeb46b3de
VLV01 <vinicius.linsvasco@hotmail.com>
2023-02-15 16:42:55 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/VLV01/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.VLV01.workshopmongo.repository;
+
+import com.VLV01.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
