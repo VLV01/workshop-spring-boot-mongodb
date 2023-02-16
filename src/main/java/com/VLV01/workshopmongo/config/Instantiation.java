700142ca5c67690a80f2a38cee4d13c47ee7408a
VLV01 <vinicius.linsvasco@hotmail.com>
2023-02-16 16:25:50 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/VLV01/workshopmongo/config/Instantiation.java
+++ src/main/java/com/VLV01/workshopmongo/config/Instantiation.java
@@ -8,6 +8,7 @@ import java.util.TimeZone;
 
 import com.VLV01.workshopmongo.domain.Post;
 import com.VLV01.workshopmongo.dto.AuthorDTO;
+import com.VLV01.workshopmongo.dto.CommentDTO;
 import com.VLV01.workshopmongo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -43,6 +44,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
